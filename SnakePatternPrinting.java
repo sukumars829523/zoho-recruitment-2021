113cd0d70e6fa8940e1a20cb2e6723b9bd2859b0
sukumars829523 <78219128+sukumars829523@users.noreply.github.com>
2021-02-01 09:48:18 +0530

    Add files via upload
    1.Write a program to print a snake matrix in the following pattern without using arrays and if conditions.

---
+++ SnakePatternPrinting.java
@@ -0,0 +1,41 @@
+package round1;
+
+import java.util.Scanner;
+
+
+// program to print snake pattern without array and if condition
+
+public class SnakePatternPrinting {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter input N: ");
+		int n = 4;//sc.nextInt();
+		
+		printSnakePattern(n);
+		sc.close();
+	}
+
+	private static void printSnakePattern(int n) {
+		int pval = 1 ;
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < 2*n-1; j++) {
+				
+				for(;j < n-i-1 ; j++)
+					System.out.print("_ ");
+				
+				for(; i%2 == 0 &&  j < n-i-1+n;j++)
+					System.out.print(pval++ +" ");
+				
+				for(; i%2 == 1 &&  j < n-i-1+n;j++)
+					System.out.print(--pval + " ");
+			}
+			pval+=n;
+			System.out.println();
+		}
+		
+	}
+
+}
